cae1c405408ae978a3e2c006f1021497fe2d3235
Krystel Baca <krystelbaca@Krystels-MacBook-Pro.local>
2024-07-07 00:16:24 -0600

    fixing build folder

--- hanoi-backend/App.js
+++ hanoi-backend/App.js
@@ -15,14 +15,12 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cors());
 app.use('/', router)
 
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '..', 'hanoi-tower-client', 'build')));
 
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'hanoi-tower-client', 'build', 'index.html'));
 });
-
 app.listen(port, () => {
   console.log(`Server listening at port ${port}`)
 })
