53d1be9641f4c2edbc18e9dee5af6394c2c2ceb3
Krystel Baca <krystelbaca@Krystels-MacBook-Pro.local>
2024-07-06 23:08:06 -0600

    Build frontend folder

--- hanoi-backend/App.js
+++ hanoi-backend/App.js
@@ -14,11 +14,11 @@ app.use(cors());
 app.use('/', router)
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
+app.use(express.static(path.join(__dirname, '..', 'tower-hanoi-client', 'build')));
 
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'tower-hanoi-client', 'build', 'index.html'));
 });
 
 app.listen(port, () => {
